ca2437e2f41ef5b3d7b9909878902d0c51b85829
sourabh94 <patil.sourabh4991@gmail.com>
2019-08-27 22:20:54 +0530

    private chat sample integration

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 const sockFolder = 'socket';
 const markers = [];
 
-var users=[];
+var users={};
 var connections = [];
 
 app.use(express.static('public'));
@@ -40,17 +40,39 @@ Socketio.on("connection", socket =>{
 	socket.on('new user',function(data,callback){
 		callback(true);
 		socket.username = data;
-		users.push(socket.username);
+		users[socket.username] = socket;
+		//users.push(socket.username);
 		updateUsernames();
 	});
 	function updateUsernames(){
-		Socketio.emit("get users", users);
+		Socketio.emit("get users", Object.keys(users));
 	}
+
+	//send message
+	socket.on("send message", data => {
+		console.log(data);
+		var msg = data.trim();
+		if (msg.substr(0,3)==='/w '){
+			msg = msg.substr(3);
+			var i = msg.indexOf(' ');
+			if(i !== -1){
+				var name = msg.substr(0,i);
+				var msg = msg.substr(i+1);
+				users[name].emit("new message", {msg:msg, user:socket.username});
+				users[socket.username].emit("new message", {msg:msg, user:socket.username});
+				console.log('whisper!');
+			}
+		}
+		else{
+			Socketio.emit("new message", {msg:msg, user:socket.username});
+		}
+	}); 
 
 	//Disconnect
 	socket.on('disconnect', function(data){
 		//if(!socket.username) return;
-		users.splice(users.indexOf(socket),1);
+		delete users[socket.username];
+		//users.splice(users.indexOf(socket),1);
 		updateUsernames();
 
 		connections.splice(connections.indexOf(socket),1);
--- Backend/socket/chat.js
+++ Backend/socket/chat.js
@@ -1,6 +1,3 @@
-socket.on("send message", data => {
-	console.log(data);
-	Socketio.emit("new message", {msg:data, user:socket.username});
-}); 
+
 
 
